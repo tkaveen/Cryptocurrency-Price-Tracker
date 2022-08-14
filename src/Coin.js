d00c4ffa5545e591379d30dc11f893d6afc7535e
Theshan Kaveen <64361534+tkaveen@users.noreply.github.com>
2022-08-14 13:06:35 +0530

    API Connection done

---
+++ src/Coin.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Coin = ({ name, image, symbol, price, volume }) => {
+  return (
+    <div className="coin-container">
+      <div className="coin-row"></div>
+      <div className="coin">
+        <img src={image} alt="crypto" />
+        <h1>{name}</h1>
+        <p className="coin-symbol">{symbol}</p>
+      </div>
+      <div className="coin-data">
+        <p className="coin-price">Rs.{price}</p>
+        <p className="coin-volume">Rs.{volume.toLocaleString()}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Coin;
